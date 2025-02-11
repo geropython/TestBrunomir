5dbe9f4aaf530256dcc0f2621a9191a4cb204318
geropython <72422102+geropython@users.noreply.github.com>
2025-02-11 02:12:34 -0300

    Mom and Dad Sprites + chat
    +third song
    +EndPanel with scene transitions

--- Assets/GameTest/Scripts/tercerScript.cs
+++ Assets/GameTest/Scripts/tercerScript.cs
@@ -2,32 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Doublsb.Dialog;
+using UnityEngine.SceneManagement;
 
 public class tercerScript : MonoBehaviour
 {
     public DialogManager DialogManager;
-    [SerializeField] private GameObject  _outSide, _livingRoom;
+    [SerializeField] private GameObject  _outSide, _livingRoom, _endPanel;
 
     private void Awake()
     {
         var dialogTexts = new List<DialogData>();
-        AddDialog(dialogTexts, "/emote:Normal//color:red//click//sound:laugh/Terminado el cafe vayamos a la mesa del living.", "Nathan");
-        var goLiving = new DialogData("/emote:Normal/Ambos se dirigen a disfrutar de su cafe en el living room.", "Narrador");
+        AddDialog(dialogTexts, "/emote:Normal//color:red//click//sound:laugh/After coffee, let's go to the living room table.", "Nathan");
+        var goLiving = new DialogData("/emote:Normal/They both head to enjoy their coffee in the living room.", "Narrador");
         goLiving.Callback = () => GoLivingRoom();
         dialogTexts.Add(goLiving); 
+        //Narrator
+        AddDialog(dialogTexts, "/emote:Normal/Nathan and Samuel enjoyed a good coffee and chatted about their lives.", "Narrador");
+        AddDialog(dialogTexts, "/emote:Normal/Until it was 8:30 and Samuel remembered that he had to go home.", "Narrador");
 
-        // Add all dialogs
         AddDialog(dialogTexts, "/emote:Nervous//color:cyan/Hi, Oh look at the time. It's too late, my mother is going to worry.I should be leaving.", "Samuel");
         AddDialog(dialogTexts, "/emote:Sad//color:red/Yes, no problem. My parents should be arriving right now..", "Nathan");
-        AddDialog(dialogTexts, "/emote:Nervous//color:cyan/Hi, Oh look at the time. It's too late, my mother is going to worry.I should be leaving.", "Samuel");
-        AddDialog(dialogTexts, "/emote:Sad//color:red/Yes, no problem. My parents should be arriving right now..", "Nathan");
-        //Mom
-        AddDialog(dialogTexts, "/emote:Normal//color:pink/Puchaina", "Mom");//TODO: MOM AND DAD
-        //Change Background to see the car parking in the garage
-        var goOutside = new DialogData("/emote:Normal/Nathan hear the car arrive at the garage.", "Narrador");
+        
+        //Garage
+        var goOutside = new DialogData("/emote:Normal/They hear the car arrive at the garage.", "Narrador");
         goOutside.Callback = () => ChangeOutside();
-        dialogTexts.Add(goOutside); 
-        dialogTexts.Add(new DialogData("/emote:Normal/This is the end of this short history.", "Narrador"));
+        dialogTexts.Add(goOutside);
+        
+        //Change Background to see the car parking in the garage
+        var goLivingAgain = new DialogData("/emote:Normal//click//sound:ring/The parents open the front door.", "Narrador");
+        goLivingAgain.Callback = () => GoLivingRoom();
+        dialogTexts.Add(goLivingAgain); 
+
+        //parents arrive 
+        AddDialog(dialogTexts, "/emote:Surprise//color:purple//click//sound:surprise/Who are you? And what are you doing in my house?", "Mom");
+        AddDialog(dialogTexts, "/emote:Confident//color:orange//click//sound:thinking/Oh, it's the new neighbor's son. How are you young man?", "Dad"); 
+
+        //Show EndPanel
+        var FinishGame = new DialogData("/emote:Normal//click//sound:ring/Here the story ends..", "Narrador");
+        FinishGame.Callback = () => EndPanel();
+        dialogTexts.Add(FinishGame);
 
         Debug.Log("Showing dialog texts");
         DialogManager.Show(dialogTexts);
@@ -48,4 +61,11 @@ public class tercerScript : MonoBehaviour
         _outSide.SetActive(true);
         _livingRoom.SetActive(false);
     }
+    private void EndPanel()
+    {
+        _endPanel.SetActive(true);
+    }
+    public void FirstScene(){SceneManager.LoadScene("FirstScene");}
+    public void SecondScene(){SceneManager.LoadScene("SecondScene");}
+    public void ThirdScene(){SceneManager.LoadScene("ThirdScene");}
 }
